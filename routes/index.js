75a5adc3467107bfbe31005f30b68b9711d5b052
ThriceGood <jonathan.norris88@hotmail.com>
2017-08-21 23:40:36 +0200

    sorry for this...

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,41 @@ var router = express.Router();
 
 // GET home page
 router.get('/', function(req, res, next) {
-  res.render('layout', {view: 'index', view_data: {hello: 'world'}});
+
+  projects = [
+    {
+      name: 'My Project 1',
+      description: 'Project Description Project Description Project Description',
+      colour: 'success'
+    },
+    {
+      name: 'My Project 2',
+      description: 'Project Description Project Description Project Description',
+      colour: 'info'
+    },
+    {
+      name: 'My Project 3',
+      description: 'Project Description Project Description Project Description',
+      colour: 'danger'
+    },
+    {
+      name: 'My Project 4',
+      description: 'Project Description Project Description Project Description',
+      colour: 'primary'
+    },
+    {
+      name: 'My Project 6',
+      description: 'Project Description Project Description Project Description',
+      colour: 'warning'
+    },
+  ]
+
+  res.render('layout', {view: 'index', view_data: {projects: projects}});
 });
 
 // GET project page
-router.get('/project', function(req, res, next) {
+router.get('/project/:name', function(req, res, next) {
+  var project_name = req.params.name.replace(/-/g, ' ');
   res.render('layout', {view: 'project', view_data: {hello: 'world'}});
 });
 
