abfdbc139ec8e34a750e29a2430ec66d47d40baa
Jonathan NORRIS <jonathan.norris@ist.ac.at>
2017-08-22 15:22:53 +0200

    full project crud

--- routes/projects.js
+++ routes/projects.js
@@ -5,41 +5,12 @@ var Project = require('../models/project.model');
 
 // GET home page
 router.get('/', function(req, res, next) {
-
-  test_projects = [
-    {
-      name: 'My Project 1',
-      description: 'Project Description Project Description Project Description',
-      colour: 'success'
-    },
-    {
-      name: 'My Project 2',
-      description: 'Project Description Project Description Project Description',
-      colour: 'info'
-    },
-    {
-      name: 'My Project 3',
-      description: 'Project Description Project Description Project Description',
-      colour: 'danger'
-    },
-    {
-      name: 'My Project 4',
-      description: 'Project Description Project Description Project Description',
-      colour: 'primary'
-    },
-    {
-      name: 'My Project 6',
-      description: 'Project Description Project Description Project Description',
-      colour: 'warning'
-    },
-  ]
-
   Project.find({}, function (err, projects) {
     res.render('layout', {view: 'projects', view_data: {projects: projects}});
   });
-
 });
 
+// create project
 router.post('/', function(req, res, next) {
   var project = new Project({
     name: req.body.name,
@@ -58,6 +29,7 @@ router.post('/', function(req, res, next) {
   })
 });
 
+// update project
 router.put('/', function(req, res, next) {
   var project = {
     name: req.body.name,
@@ -76,6 +48,7 @@ router.put('/', function(req, res, next) {
   });
 });
 
+// delete project
 router.delete('/:project_name', function(req, res, next) {
   Project.remove({name: req.params.project_name}, function(err) {
       if (err) {
